6ad8636907b9619e3d57ed8aef0edaadc3d36c50
Frank Breedijk <f.breedijk@divd.nl>
2023-06-21 20:00:45 +0200

    Adding the ability to rest HTML code

---
+++ test_it.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+docker run \
+	--volume="$PWD:/root/project:delegated" \
+	--entrypoint /bin/sh \
+	-ti node:20-alpine \
+	-c "
+		cd /root/project/;
+		yarn
+		yarn build
+
+		apk update
+		apk add py3-pip ruby ruby-dev ruby-nokogiri alpine-sdk openjdk11-jre-headless
+		pip3 install html5validator 
+		gem install --no-document html-proofer
+		./proof_html.sh
+"
